d1d06d47211261f6ceb6b6bbbe912ded0a03b20d
Hiromune Ito <muniere.fujiwara@gmail.com>
2022-03-07 22:04:22 +0900

    Fix how to color fill and stroke

--- lib/src/main/java/net/muniere/sketchbook/lib/processing/Graphics.kt
+++ lib/src/main/java/net/muniere/sketchbook/lib/processing/Graphics.kt
@@ -4,13 +4,13 @@ import android.graphics.Color
 import processing.core.PGraphics
 
 public fun PGraphics.background(color: Color) {
-  this.background(color.red(), color.green(), color.blue())
+  this.background(color.red() * 255, color.green() * 255, color.blue() * 255)
 }
 
 public fun PGraphics.stroke(color: Color) {
-  this.stroke(color.red(), color.green(), color.blue())
+  this.stroke(color.red() * 255, color.green() * 255, color.blue() * 255)
 }
 
 public fun PGraphics.fill(color: Color) {
-  this.fill(color.red(), color.green(), color.blue())
+  this.fill(color.red() * 255, color.green() * 255, color.blue() * 255)
 }
