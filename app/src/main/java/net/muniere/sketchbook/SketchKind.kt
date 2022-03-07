e37c6339b57f1a114dd09264f4e0c217c816b840
Hiromune Ito <muniere.fujiwara@gmail.com>
2022-03-07 22:11:25 +0900

    Remove example sketch

--- app/src/main/java/net/muniere/sketchbook/SketchKind.kt
+++ app/src/main/java/net/muniere/sketchbook/SketchKind.kt
@@ -3,17 +3,11 @@ package net.muniere.sketchbook
 public enum class SketchKind(
   public val rawValue: Int,
 ) {
-  EXAMPLE(0),
   STAR_FIELD(1),
   MENGER_SPONGE(2),;
 
   public val seed: SketchSeed
     get() = when (this) {
-      EXAMPLE -> SketchSeed(
-        kind = this,
-        title = "Example",
-        caption = "Example",
-      )
       STAR_FIELD -> SketchSeed(
         kind = this,
         title = "Star Field",
--- app/src/main/java/net/muniere/sketchbook/Sketches.kt
+++ app/src/main/java/net/muniere/sketchbook/Sketches.kt
@@ -1,16 +1,14 @@
 package net.muniere.sketchbook
 
-import net.muniere.sketchbook.example.ExampleSketch
 import net.muniere.sketchbook.lib.graphics.Size2D
 import net.muniere.sketchbook.lib.processing.Sketch
-import net.muniere.sketchbook.orb.starField.Sketch as StarFieldSketch
 import net.muniere.sketchbook.orb.mengerSponge.Sketch as MengerSpongeSketch
+import net.muniere.sketchbook.orb.starField.Sketch as StarFieldSketch
 
 public final object Sketches {
 
   public fun create(kind: SketchKind, size: Size2D): Sketch {
     return when (kind) {
-      SketchKind.EXAMPLE -> ExampleSketch(size)
       SketchKind.STAR_FIELD -> StarFieldSketch(size)
       SketchKind.MENGER_SPONGE -> MengerSpongeSketch(size)
     }
