b69eb593f289e75e6b5a3051c788f12258c68b62
Hiromune Ito <muniere.fujiwara@gmail.com>
2022-03-08 18:17:09 +0900

    Update build.gradle for configureondemand

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,10 @@ plugins {
 tasks.register("clean", type = Delete::class) {
   delete(rootProject.buildDir)
 }
+
+// https://github.com/gradle/gradle/issues/4823#issuecomment-715615422
+subprojects {
+  if (gradle.startParameter.isConfigureOnDemand && buildscript.sourceFile?.extension?.toLowerCase() == "kts" && parent != rootProject) {
+    generateSequence(parent) { it.parent }.filter { it != rootProject }.forEach { evaluationDependsOn(it.path) }
+  }
+}
