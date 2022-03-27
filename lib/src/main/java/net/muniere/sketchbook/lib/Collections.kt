515cc310bf9b3bca1f41d4b309cad54d505d2466
Hiromune Ito <muniere.fujiwara@gmail.com>
2022-03-27 12:03:47 +0900

    Enable to generate permutation from Iterable

---
+++ lib/src/main/java/net/muniere/sketchbook/lib/Collections.kt
@@ -0,0 +1,45 @@
+package net.muniere.sketchbook.lib
+
+public fun <T> MutableList<T>.swap(i: Int, j: Int) {
+  val tmp = this[i]
+  this[i] = this[j]
+  this[j] = tmp
+}
+
+public fun <T> MutableList<T>.reverse(fromIndex: Int, toIndex: Int) {
+  this.subList(fromIndex, toIndex).reversed().forEachIndexed { index, value ->
+    this[fromIndex + index] = value
+  }
+}
+
+public fun <T> MutableList<T>.reverseBefore(index: Int) {
+  this.reverse(fromIndex = 0, toIndex = index)
+}
+
+public fun <T> MutableList<T>.reverseAfter(index: Int) {
+  this.reverse(fromIndex = index, toIndex = this.size)
+}
+
+public fun <T : Comparable<T>> Collection<T>.permutation(): Sequence<List<T>> = sequence {
+  val seq = this@permutation.toMutableList()
+
+  yield(seq.toList())
+
+  while (true) {
+    val x = seq.zipWithNext().indexOfLast { it.first < it.second }
+    if (x < 0) {
+      break
+    }
+
+    val v = seq[x]
+    val y = seq.withIndex().indexOfLast { it.index > x && it.value > v }
+    if (y < 0) {
+      break
+    }
+
+    seq.swap(x, y)
+    seq.reverseAfter(x + 1)
+
+    yield(seq.toList())
+  }
+}
